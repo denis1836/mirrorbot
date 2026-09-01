e4eab5d2510cdcae93c194f22a76cbb1e9dc9710
denis1836 <den.pylypen@protonmail.com>
2026-09-01 22:04:20 +0200

    feat(gitlab): add client creation

--- internal/gitlab/client.go
+++ internal/gitlab/client.go
@@ -1,1 +1,31 @@
 package gitlab
+
+import (
+	"fmt"
+
+	gitlab "gitlab.com/gitlab-org/api/client-go"
+)
+
+type Client struct {
+	sdk       *gitlab.Client
+	projectID string
+}
+
+func NewClient(token, projectID string) (*Client, error) {
+	if token == "" {
+		return nil, fmt.Errorf("gitlab token cannot be empty")
+	}
+	if projectID == "" {
+		return nil, fmt.Errorf("gitlab project ID cannot be empty")
+	}
+
+	git, err := gitlab.NewClient(token)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create gitlab client: %w", err)
+	}
+
+	return &Client{
+		sdk:       git,
+		projectID: projectID,
+	}, nil
+}

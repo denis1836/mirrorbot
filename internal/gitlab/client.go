12610d99aa480a650a2ae41a36ecb69dada8d32d
denis1836 <den.pylypen@protonmail.com>
2026-09-02 20:29:28 +0200

    feat(gitlab): add issue creating function

--- internal/gitlab/client.go
+++ internal/gitlab/client.go
@@ -1,6 +1,7 @@
 package gitlab
 
 import (
+	"context"
 	"fmt"
 
 	gitlab "gitlab.com/gitlab-org/api/client-go"
@@ -28,4 +29,25 @@ func NewClient(token, projectID string) (*Client, error) {
 		sdk:       git,
 		projectID: projectID,
 	}, nil
+}
+
+type CreateIssueInput struct {
+	Title       string
+	Description string
+	Labels      []string
+}
+
+func (c *Client) CreateIssue(ctx context.Context, input CreateIssueInput) (*gitlab.Issue, error) {
+	opts := &gitlab.CreateIssueOptions{
+		Title:       gitlab.Ptr(input.Title),
+		Description: gitlab.Ptr(input.Description),
+		Labels:      gitlab.Ptr(gitlab.LabelOptions(input.Labels)),
+	}
+
+	issue, _, err := c.sdk.Issues.CreateIssue(c.projectID, opts, gitlab.WithContext(ctx))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create a gitlab issue: %w", err)
+	}
+
+	return issue, nil
 }

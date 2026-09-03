75203cc253d130af685ac049a99042c72ec7af49
denis1836 <den.pylypen@protonmail.com>
2026-09-03 21:47:29 +0200

    feat(sync): add gh actions events switcher for gitlab

---
+++ internal/sync/engine.go
@@ -0,0 +1,29 @@
+package sync
+
+import (
+	"context"
+	"fmt"
+
+	"mirrorbot/internal/gitlab"
+)
+
+type Engine struct {
+	gitlabClient *gitlab.Client
+}
+
+func NewEngine(gl *gitlab.Client) *Engine {
+	return &Engine{gitlabClient: gl}
+}
+
+func (e *Engine) Dispatch(ctx context.Context, eventName string, payload []byte) error {
+	var processor EventProcessor
+
+	switch eventName {
+	case "issues":
+		processor = NewIssueProcessor(e.gitlabClient)
+	default:
+		return fmt.Errorf("unsupported event type: %w", eventName)
+	}
+
+	return processor.Process(ctx, payload)
+}

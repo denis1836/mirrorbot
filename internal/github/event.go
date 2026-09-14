e7551987b290b72419ee230b759b249c82a69dd6
denis1836 <den.pylypen@protonmail.com>
2026-09-14 21:45:41 +0200

    feat(event/pr): add github opened pull request input

--- internal/github/event.go
+++ internal/github/event.go
@@ -41,3 +41,42 @@ func ParseIssuesEvent(rawPayload []byte) (*IssueInput, error) {
 		Labels:      labels,
 	}, nil
 }
+
+type PullRequestInput struct {
+	Title        string
+	Description  string
+	SourceBranch string
+	TargetBranch string
+	Labels       []string
+}
+
+func ParsePullRequestEvent(rawPayload []byte) (*PullRequestInput, error) {
+	var event github.PullRequestEvent
+	if err := json.Unmarshal(rawPayload, &event); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal github pull request event: %w", err)
+	}
+
+	if event.GetAction() != "opened" {
+		return nil, fmt.Errorf("unsupported action %v, only 'opened' is supported", event.GetAction())
+	}
+
+	pr := event.GetPullRequest()
+	if pr == nil {
+		return nil, fmt.Errorf("event payload missing pull request object")
+	}
+
+	var labels []string
+	for _, label := range pr.Labels {
+		if name := label.GetName(); name != "" {
+			labels = append(labels, name)
+		}
+	}
+
+	return &PullRequestInput{
+		Title:        pr.GetTitle(),
+		Description:  pr.GetBody(),
+		SourceBranch: pr.GetHead().GetRef(),
+		TargetBranch: pr.GetBase().GetRef(),
+		Labels:       labels,
+	}, nil
+}
